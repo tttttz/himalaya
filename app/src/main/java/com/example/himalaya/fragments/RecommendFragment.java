bc4181787b2d359cf759d6e31d14baa7722ecef4
tzbibi <894661727@qq.com>
2020-01-17 22:46:51 +0800

    获取推荐数据

--- app/src/main/java/com/example/himalaya/fragments/RecommendFragment.java
+++ app/src/main/java/com/example/himalaya/fragments/RecommendFragment.java
@@ -6,12 +6,61 @@ import android.view.ViewGroup;
 
 import com.example.himalaya.R;
 import com.example.himalaya.base.BaseFragment;
+import com.example.himalaya.utils.Constants;
+import com.example.himalaya.utils.LogUtil;
+import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.album.Album;
+import com.ximalaya.ting.android.opensdk.model.album.GussLikeAlbumList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class RecommendFragment extends BaseFragment {
 
+    private static final String TAG = "RecommendFragment";
+
     @Override
     protected View onSubViewLoad(LayoutInflater layoutInflater, ViewGroup container) {
+        //view加载完成
         View rootView = layoutInflater.inflate(R.layout.fragment_recommend, container, false);
+
+        //获取数据
+        getRecommendData();
+
+        //返回view给界面显示
         return rootView;
+    }
+
+    /**
+     * 获取推荐内容（猜你喜欢）
+     * 接口：3.10.6 获取猜你喜欢专辑
+     */
+    private void getRecommendData() {
+        //封装参数
+        Map<String, String> map = new HashMap<>();
+        //这个参数表示一页数据返回多少条
+        map.put(DTransferConstants.LIKE_COUNT, Constants.RECOMMEND_COUNT + "");
+        CommonRequest.getGuessLikeAlbum(map, new IDataCallBack<GussLikeAlbumList>() {
+            @Override
+            public void onSuccess(GussLikeAlbumList gussLikeAlbumList) {
+                //数据获取成功
+                if (gussLikeAlbumList != null) {
+                    List<Album> albumList = gussLikeAlbumList.getAlbumList();
+                    if (albumList != null) {
+                        LogUtil.d(TAG, "size -- >" + albumList.size());
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                //数据获取失败
+                LogUtil.d(TAG,"error -- >" + i);
+                LogUtil.d(TAG,"errorMsg -- >" + s);
+            }
+        });
     }
 }
