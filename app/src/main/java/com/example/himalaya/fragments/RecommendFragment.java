62cfbb61c2f3c3bda4277e48a402172b7fb02639
tzbibi <894661727@qq.com>
2020-01-20 21:50:51 +0800

    获取推荐数据

--- app/src/main/java/com/example/himalaya/fragments/RecommendFragment.java
+++ app/src/main/java/com/example/himalaya/fragments/RecommendFragment.java
@@ -4,7 +4,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.himalaya.R;
+import com.example.himalaya.adapters.RecommendListAdapter;
 import com.example.himalaya.base.BaseFragment;
 import com.example.himalaya.utils.Constants;
 import com.example.himalaya.utils.LogUtil;
@@ -21,17 +25,30 @@ import java.util.Map;
 public class RecommendFragment extends BaseFragment {
 
     private static final String TAG = "RecommendFragment";
+    private View mRootView;
+    private RecyclerView mRecommendRV;
+    private RecommendListAdapter mRecommendListAdapter;
 
     @Override
     protected View onSubViewLoad(LayoutInflater layoutInflater, ViewGroup container) {
         //view加载完成
-        View rootView = layoutInflater.inflate(R.layout.fragment_recommend, container, false);
+        mRootView = layoutInflater.inflate(R.layout.fragment_recommend, container, false);
 
+        //RecyclerView的使用
+        //1.找到对应的控件
+        mRecommendRV = mRootView.findViewById(R.id.recommend_list);
+        //2.设置布局管理器
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        mRecommendRV.setLayoutManager(linearLayoutManager);
+        //3.设置适配器
+        mRecommendListAdapter = new RecommendListAdapter();
+        mRecommendRV.setAdapter(mRecommendListAdapter);
         //获取数据
         getRecommendData();
 
         //返回view给界面显示
-        return rootView;
+        return mRootView;
     }
 
     /**
@@ -49,9 +66,8 @@ public class RecommendFragment extends BaseFragment {
                 //数据获取成功
                 if (gussLikeAlbumList != null) {
                     List<Album> albumList = gussLikeAlbumList.getAlbumList();
-                    if (albumList != null) {
-                        LogUtil.d(TAG, "size -- >" + albumList.size());
-                    }
+                    //数据回来后要更新UI
+                    updateRecommendUI(albumList);
                 }
             }
 
@@ -62,5 +78,11 @@ public class RecommendFragment extends BaseFragment {
                 LogUtil.d(TAG,"errorMsg -- >" + s);
             }
         });
+    }
+
+    //将数据展示更新到UI
+    private void updateRecommendUI(List<Album> albumList) {
+        //把数据设置给适配器并更新UI
+        mRecommendListAdapter.setData(albumList);
     }
 }
--- app/src/main/java/com/example/himalaya/utils/Constants.java
+++ app/src/main/java/com/example/himalaya/utils/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 
 
     //获取推荐列表的专辑数量
-    public static int RECOMMEND_COUNT = 20;
+    public static int RECOMMEND_COUNT = 40;
 
 }
